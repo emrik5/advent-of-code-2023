1b6255a3ec3d1cd83cc6aa6af3ffc512d552c4a9
emrik5 <emrikulwan@gmail.com>
2023-12-05 14:07:47 +0100

    fuk

--- src/first.rs
+++ src/first.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::{self, read_to_string},
-    io::Result,
+    io::{Result, Write},
 };
 
 pub fn first_p1() -> Result<u32> {
@@ -26,4 +26,56 @@ pub fn first_p1() -> Result<u32> {
         })
         .sum();
     Ok(inp)
+}
+
+pub fn first_p2() -> Result<u32> {
+    let nums = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ]
+    .into_iter();
+    let inp: Vec<_> = read_to_string("input/first.txt")?
+        .lines()
+        .map(|line| {
+            let mut temp = String::from(line);
+            nums.clone().enumerate().for_each(|(i, num)| {
+                temp = temp.replacen(num, &i.to_string(), 1).to_owned();
+            });
+            nums.clone().enumerate().for_each(|(i, num)| {
+                temp = temp
+                    .chars()
+                    .rev()
+                    .collect::<String>()
+                    .replacen(&num.chars().rev().collect::<String>(), &i.to_string(), 1)
+                    .chars()
+                    .rev()
+                    .collect::<String>()
+            });
+            temp.chars()
+                .filter(|&char| char.is_ascii_digit())
+                .collect::<String>()
+        })
+        .collect();
+    let inp: Vec<_> = inp
+        .into_iter()
+        .map(|line| {
+            let len = line.len();
+            let mut line = line.chars();
+            print!("{:?}", line);
+            let ret = if len == 1 {
+                String::from_iter([line.next().unwrap(); 2])
+            } else {
+                String::from_iter([line.next().unwrap(), line.last().unwrap()])
+            }
+            .parse::<u32>()
+            .unwrap();
+            println!("{:?}", ret);
+            ret
+        })
+        .collect();
+    println!("AAAAAAAAAAAAaaa");
+    println!("e : {:?}", inp);
+    println!("sum : {:?}", inp.iter().sum::<u32>());
+    // .sum();
+
+    Ok(1)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::error::Error;
 
-use first::first_p1;
+use first::first_p2;
 
 pub mod first;
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("{}", first_p1()?);
+    println!("{}", first_p2()?);
     Ok(())
 }
